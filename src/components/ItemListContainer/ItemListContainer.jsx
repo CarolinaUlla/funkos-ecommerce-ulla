f2cdbd55553b464182d04a7d77788c3c257b14dc
caro <c.ulla@viseven.com>
2022-04-21 15:28:46 -0300

    editing ItemListContainer component

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import './ItemListContainer.css'
+import ItemCount from '../ItemCount/ItemCount'
 
 export default function ItemListContainer({greeting}) {
   return (
-    <div>
-        <p className='greeting'>{greeting}</p>
+  <>
+  <div className='greeting'>
+    <p>{greeting}</p>
     </div>
+    <ItemCount></ItemCount>
+  </>
   )
 }
